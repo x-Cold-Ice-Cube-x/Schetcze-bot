7e6291deafb15e25b2a78d7a9c1729e95e3ee06d
Artem Bush <artembushagin@mail.ru>
2024-01-01 23:48:00 +0300

    Структурные нововведения: 1) Создание класса Text (text.py): хранение всех текстовых полей 2) Создание абстрактного класса Table (table.py): методы взаимодействия с таблицами sqlite баз данных

---
+++ configs/text.py
@@ -0,0 +1,5 @@
+class Text:
+    # ---------- Пути к файлам --------- #
+    databaseFilepath = f"databases/bot_database.sqlite"  # относительный путь к Sqlite базе
+    logsFilepath = f"databases/logs.log"  # относительный путь к логам
+    # ---------------------------------- #
